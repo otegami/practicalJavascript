8250b470e5420ca00b02425fef76a745e26829c8
otegami <a.s.takuya1026@gmail.com>
2020-05-08 16:09:18 +0900

    [Review] About the basic javascript

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
+var todoList = {
+  todos: ['item1', 'item2', 'item3'],
+  displayTodos: function() {
+    console.log('My Todos', this.todos);
+  },
+  addTodo: function(todo) {
+    this.todos.push(todo);
+    this.displayTodos();
+  }
+};
+
+// We want to get access to the display todos button
 var displayTodosButton = document.getElementById('displayTodosButton');
-var number = 10;
 console.log(displayTodosButton);
-console.log(number);
